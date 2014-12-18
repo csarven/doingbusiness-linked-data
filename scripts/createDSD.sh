62bcba15636c5df612fafdf75ebe7b9ba93d04ba
Sarven Capadisli <info@csarven.ca>
2014-12-18 17:11:12 +0100

    Refactor

--- scripts/createDSD.sh
+++ scripts/createDSD.sh
@@ -4,7 +4,7 @@
 #variables
 codeIndicators=(ease-of-doing-business dealing-with-construction-permits enforcing-contracts getting-credit getting-electricity paying-taxes protecting-minority-investors registering-property resolving-insolvency starting-a-business trading-across-borders);
 codeIndicatorLabels=("Ease of Doing Business" "Dealing with Construction Permits" "Enforcing Contracts" "Getting Credit" "Getting Electricity" "Paying Taxes" "Protecting Minority Investors" "Registering Property" "Resolving Insolvency" "Starting a Business" "Trading Across Borders");
-creators=("<http://renatostauffer.ch>" "<http://csarven.ca/#i>");
+creators=("<http://renatostauffer.ch/>" "<http://csarven.ca/#i>");
 licence="<http://creativecommons.org/publicdomain/zero/1.0/>";
 componentsEaseOfDoingBusiness=(overall-dtf);
 componentsDealingWithConstructionPermits=(procedures days cost-in-percent-of-warehouse-value);
@@ -49,153 +49,155 @@ printf "\n" >> meta.ttl;
 
 
 #loop every indicator to create the DataStructureDefinition and the dataset
-arrayLenght=${#codeIndicators[@]}
+arrayLength=${#codeIndicators[@]}
 
-for ((i=0; i<${arrayLenght}; i++));
+for ((i=0; i<${arrayLength}; i++));
 do
-	echo "#" >> meta.ttl;
-	echo "# ${codeIndicatorLabels[$i]} dataset" >> meta.ttl;
-	echo "#" >> meta.ttl;
-	printf "\n" >> meta.ttl;
-
-	echo "dataset:${codeIndicators[$i]}
-	qb:structure structure:${codeIndicators[$i]} ;
-	rdfs:label \"Dooing Business - ${codeIndicatorLabels[$i]} dataset\"@en ;" >> meta.ttl;
-
-	if [ ${codeIndicators[$i]} != ease-of-doing-business ]
-    	then
-			echo "\tfoaf:page <http://www.doingbusiness.org/data/exploretopics/${codeIndicators[$i]}> ;" >> meta.ttl;
-    	fi
-
-	echo "\tdcterms:issued \"2014-12-04T00:00:00Z\"^^xsd:dateTime ;
-	dcterms:modified \"2014-12-04T00:00:00Z\"^^xsd:dateTime ;" >> meta.ttl;
-    printf "\tdcterms:creator <http://renatostauffer.ch>,\n\t\t\t<http://csarven.ca/#i> ;\n" >> meta.ttl;
-    echo "\tdcterms:license $licence ;" >> meta.ttl;
-    echo "." >> meta.ttl;
+    echo "#" >> meta.ttl;
+    echo "# ${codeIndicatorLabels[$i]} dataset" >> meta.ttl;
+    echo "#" >> meta.ttl;
     printf "\n" >> meta.ttl;
+
+    echo "dataset:${codeIndicators[$i]}
+    a qb:DataSet ;
+    qb:structure structure:${codeIndicators[$i]} ;
+    dcterms:title \"${codeIndicatorLabels[$i]}\"@en ;" >> meta.ttl;
+
+    if [ ${codeIndicators[$i]} != ease-of-doing-business ]
+        then
+            echo "    foaf:page <http://www.doingbusiness.org/data/exploretopics/${codeIndicators[$i]}> ;" >> meta.ttl;
+        fi
+
+    echo '    dcterms:issued "2014-12-04T00:00:00Z"^^xsd:dateTime ;
+    dcterms:modified "2014-12-04T00:00:00Z"^^xsd:dateTime ;
+    dcterms:creator
+        <http://renatostauffer.ch/> ,
+        <http://csarven.ca/#i> ;
+    dcterms:license '"${licence}"' ;
+    .'>> meta.ttl;
 
     echo "structure:${codeIndicators[$i]}
     a qb:DataStructureDefinition ;
     qb:component component:rank ;" >> meta.ttl;
 
     if [ ${codeIndicators[$i]} != ease-of-doing-business ]
-    	then
-			echo "\tqb:component component:dtf ;" >> meta.ttl;
-    	fi
+        then
+            echo "    qb:component component:dtf ;" >> meta.ttl;
+        fi
     
-    echo "\tqb:component component:economy ;
+    echo "    qb:component component:economy ;
     qb:component component:refPeriod ;
     qb:component component:indicator-${codeIndicators[$i]} ;" >> meta.ttl;
 
     #loop through each component of each code-indicator
     case "${codeIndicators[$i]}" in 
-    	dealing-with-construction-permits)
-			for component in "${componentsDealingWithConstructionPermits[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		enforcing-contracts)
-			for component in "${componentsEnforcingContraacts[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		getting-credit)
-			for component in "${componentsGettingCredit[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		getting-electricity)
-			for component in "${componentsGettingElectricity[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		paying-taxes)
-			for component in "${componentsPayingTaxes[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		protecting-minority-investors)
-			for component in "${componentsProtectingMinorityInvestors[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		registering-property)
-			for component in "${componentsRegisteringProperty[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		resolving-insolvency)
-			for component in "${componentsResolvingInsolvency[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		starting-a-business)
-			for component in "${componentsStartingABusiness[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		trading-across-borders)
-			for component in "${componentsTradingAcrossBorders[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;
-		ease-of-doing-business)
-			for component in "${componentsEaseOfDoingBusiness[@]}"
-				do
-					echo "\tqb:component component:$component ;" >> meta.ttl;
-				done
-			;;			
-		*) echo "Error: This code-indicator does not exist..." >> meta.ttl;
-			;;	
-	esac
-	echo "." >> meta.ttl;
-	printf "\n" >> meta.ttl;
+        dealing-with-construction-permits)
+            for component in "${componentsDealingWithConstructionPermits[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        enforcing-contracts)
+            for component in "${componentsEnforcingContraacts[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        getting-credit)
+            for component in "${componentsGettingCredit[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        getting-electricity)
+            for component in "${componentsGettingElectricity[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        paying-taxes)
+            for component in "${componentsPayingTaxes[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        protecting-minority-investors)
+            for component in "${componentsProtectingMinorityInvestors[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        registering-property)
+            for component in "${componentsRegisteringProperty[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        resolving-insolvency)
+            for component in "${componentsResolvingInsolvency[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        starting-a-business)
+            for component in "${componentsStartingABusiness[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        trading-across-borders)
+            for component in "${componentsTradingAcrossBorders[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;
+        ease-of-doing-business)
+            for component in "${componentsEaseOfDoingBusiness[@]}"
+                do
+                    echo "    qb:component component:$component ;" >> meta.ttl;
+                done
+            ;;            
+        *) echo "Error: This code-indicator does not exist..." >> meta.ttl;
+            ;;    
+    esac
+    echo "." >> meta.ttl;
+    printf "\n" >> meta.ttl;
 
-	echo "code-indicator:${codeIndicators[$i]}
-	a skos:Concept ;
-	skos:inScheme code:indicator ;
-	skos:topConceptOf code:indicator ;
-	#TODO: Add definition
-	skos:definition \"\"@en ;
+    echo "code-indicator:${codeIndicators[$i]}
+    a skos:Concept ;
+    skos:inScheme code:indicator ;
+    skos:topConceptOf code:indicator ;
+    #TODO: Add definition
+    skos:definition \"\"@en ;
 ." >> meta.ttl;
     printf "\n" >> meta.ttl;
 done
 
 #create the indicators
 echo "code:indicator
-	skos:prefLabel \"Indicator Concept Scheme\"@en ;
-	skos:hasTopConcept" >> meta.ttl;
+    skos:prefLabel \"Indicator Concept Scheme\"@en ;
+    skos:hasTopConcept" >> meta.ttl;
 #loop through every code-indicator and add it to skos:hasTopConcept.
-for ((i=0; i<${arrayLenght}; i++));
+for ((i=0; i<${arrayLength}; i++));
 do
-	printf "\t\tcode-indicator:${codeIndicators[$i]}" >> meta.ttl;
-	if [ $i == $(($arrayLenght-1)) ]
-		then
-			printf " ;\n" >> meta.ttl;
-		else
-			printf ",\n" >> meta.ttl;
-		fi
+    printf "        code-indicator:${codeIndicators[$i]}" >> meta.ttl;
+    if [ $i == $(($arrayLength-1)) ]
+        then
+            printf " ;\n" >> meta.ttl;
+        else
+            printf ",\n" >> meta.ttl;
+        fi
 done
 echo "." >> meta.ttl;
 printf "\n" >> meta.ttl;
 
 #create the components for the indicators
-for ((i=0; i<${arrayLenght}; i++));
+for ((i=0; i<${arrayLength}; i++));
 do
-	echo "component:indicator-${codeIndicators[$i]}
-	a qb:ComponentSpecification ;
-	qb:dimension code-indicator:${codeIndicators[$i]} ;
-	." >> meta.ttl;
+    echo "component:indicator-${codeIndicators[$i]}
+    a qb:ComponentSpecification ;
+    qb:dimension code-indicator:${codeIndicators[$i]} ;
+    ." >> meta.ttl;
 done
 
 #Create components measures and dimensions
@@ -217,17 +219,17 @@ otherComponentsLength=${#otherComponents[@]};
 
 for ((i=0; i<${otherComponentsLength}; i++));
 do
-	echo "component:${otherComponents[$i]}
-	a qb:ComponentSpecification ;
-	qb:measure measure:${otherComponents[$i]} ;
+    echo "component:${otherComponents[$i]}
+    a qb:ComponentSpecification ;
+    qb:measure measure:${otherComponents[$i]} ;
     ." >> meta.ttl;
-	printf "\n" >> meta.ttl;
+    printf "\n" >> meta.ttl;
 
-	echo "measure:${otherComponents[$i]}
-	a qb:MeasureProperty ;
+    echo "measure:${otherComponents[$i]}
+    a qb:MeasureProperty ;
     rdfs:label \"\"@en ;
     #TODO: Add range
     rdfs:range \"Has no range yet.\"@en ;
-	." >> meta.ttl;
-	printf "\n" >> meta.ttl;
+    ." >> meta.ttl;
+    printf "\n" >> meta.ttl;
 done
