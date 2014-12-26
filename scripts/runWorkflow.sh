8930f6707f4517acb15b31f9b6f83cee3b5abbb3
reni99 <renato.stauffer90@googlemail.com>
2014-12-27 00:35:14 +0100

    Runs the entire workflow

---
+++ scripts/runWorkflow.sh
@@ -0,0 +1,16 @@
+#Abstract workflow file
+. ./config.sh
+
+#TODO: Contributor as defaut variable and possible user input!
+#Setup
+date=`date +%Y-%m-%d:%H:%M:%S`
+
+echo "<$workflowTemplate/> a opmw:WorkflowTemplate ;
+rdfs:label \"Workflow $agencyLabel\"@en ;
+dc:contributor <http://renatostauffer.ch> ;
+dc:creator <http://renatostauffer.ch> ;
+." >> $abstractWorkflowDescription;
+printf "\n" >> $abstractWorkflowDescription;
+. ./doingbusiness.get.sh
+. ./doingbusiness.preprocessing.sh
+#. ./doingbusiness.mapping.sh
